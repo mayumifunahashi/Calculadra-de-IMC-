fe4eca14946bf4923e658dfaa41c465acf05e3c9
Mayumi Funahashi <100293266+mayumifunahashi@users.noreply.github.com>
2022-05-12 21:52:57 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,125 @@
-const primeiraTela = document.getElementById('container-introdutorio')
-const segundaTela = document.getElementById('container-princial')
+
+ 
+const firstDiv = document.querySelector('#container-introdutorio');
+    const secondDiv = document.querySelector('#container-principal');
+    
+
+    function go(currentStep,nextStep)
+    {
+        let dNone, dBlock;
+        if(currentStep == 1)
+        {
+            dNone = firstDiv;
+        }
+        else if(currentStep == 2)
+        {
+            dNone = secondDiv;
+        }
+        
+        
+        dNone.style.display = 'none';
+
+        if(nextStep == 1)
+        {
+            dBlock = firstDiv;
+        }
+        else if(nextStep == 2)
+        {
+            dBlock = secondDiv;
+        }
+        
+        dBlock.style.display = 'block';
+    }
 
 
-function go (divAtual, divSeguinte) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function go (comeco, final) {
+    let = dNone, dBlcock;
+
+    if (comeco == 1){
+        dNone = primeiraTela;
+
+    } else (comeco == 2) {
+        dNone = segundaTela;
+    }
+
+    dNone.style.display = 'none';
+
+
+    if (final == 1){
+        dBlock = primeiraTela;
+
+    } else (final == 2) {
+        dBlock = segundaTela;
+    }
+
+    dBlock.style.display = 'block';
+
+
+    const primeiraTela = document.getElementById('container-introdutorio');
+const segundaTela = document.getElementById('container-princial');
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+/*function go (divAtual, divSeguinte) {
     let = displayNone, displayBlock;
-    if(divAtual ==1) {
-        displayNone = primeiraTela;
-    } else  (divAtual ==2) {
-        displayNone = divSeguinte;
+
+    /if(divAtual ==1) {
+        displayNone = segundaTela;
+    } else  (divAtual == 2) {
+        displayBlock = divSeguinte;
     }
     
-    displayNone.style.display = 'none'
+    displayNone.style.display = 'none';
      
     if(divSeguinte ==2) {
         displayBlock = primeiraTela;
@@ -23,6 +132,6 @@ function go (divAtual, divSeguinte) {
 
 function validacao {
    const nome = document.getElementById('nome');
-   const altura = document.getElementById('altura')
+   const altura = document.getElementById('altura');
    
-}
+} */
